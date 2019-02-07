50f5be1a5463b1d2ed5af28fb785c80b907fcf35
ryoya_ito <horusu.dqx@gmail.com>
2019-02-07 14:41:18 +0900

    ローカル通知メソッドにコメント作成，通知テスト用トリガー作成

--- TaskManagerApp/TaskNotification.swift
+++ TaskManagerApp/TaskNotification.swift
@@ -13,6 +13,7 @@ class TaskNotification {
     
     private init() {}
     
+    // ユーザに通知の許可をリクエストするメソッド
     static func requestNotificationPermission() {
         let center = UNUserNotificationCenter.current()
         center.requestAuthorization(options: [.badge, .sound, .alert], completionHandler: { (granted, error) in
@@ -28,11 +29,13 @@ class TaskNotification {
         })
     }
     
+    // ローカル通知をセットするメソッド
     static func setNotification(_ task: Task) {
         
         let dateComponents: DateComponents = Calendar.current.dateComponents(in: TimeZone.current, from: task.dueDate)
         
         let trigger = UNCalendarNotificationTrigger(dateMatching: dateComponents, repeats: false)
+//        let trigger = TaskNotification.triggerForTest()
         
         let content = UNMutableNotificationContent()
         content.title = "指定日です。"
@@ -40,12 +43,26 @@ class TaskNotification {
         content.body = "\(task.content)"
         content.sound = UNNotificationSound.default
         
-        let request: UNNotificationRequest = UNNotificationRequest(identifier: "onDayOfTask", content: content, trigger: trigger)
+        let request: UNNotificationRequest = UNNotificationRequest(identifier: "\(task.title)", content: content, trigger: trigger)
         
         let notificationCenter: UNUserNotificationCenter = UNUserNotificationCenter.current()
         notificationCenter.add(request, withCompletionHandler: { (error) in
+            if let error = error {
+              fatalError("\(error)")
+            }
             print("set notification")
         })
+    }
+    
+    // 通知をセットした10秒後に発火するトリガーを作成します。
+    private static func triggerForTest() -> UNTimeIntervalNotificationTrigger {
+        // trigger
+        let seconds = 10
+        let trigger = UNTimeIntervalNotificationTrigger(timeInterval: Double(seconds),
+                                                                repeats: false)
+        
+        return trigger
+
     }
 }
 
